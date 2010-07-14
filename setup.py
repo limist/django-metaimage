dc5649c70974189541f050da9335c8ea3d1c6d7c
Kai <k@limist.com>
2010-07-14 16:37:07 -0400

    Added classifiers metadata, some other metadata for a better PyPI entry.

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = 'django-metaimage',
@@ -6,8 +10,21 @@ setup(
     url = '',
     license = 'GPLv3',
     description = 'Provides a useful subclass of photologue.ImageModel.',
+    long_description = read('README'),
     author = 'Kai Wu',
+    author_email = 'k@limist.com',
+
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires = ['setuptools'],
+
+    classifiers = [
+	'Development Status :: 3 - Alpha',
+	'Framework :: Django',
+	'Intended Audience :: Developers',
+	'License :: OSI Approved :: GNU General Public License (GPL)',
+	'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+	]
     )
