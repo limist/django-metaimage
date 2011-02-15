4b005ad45d541064431a64d911086ccb58ca492e
Kai <k@limist.com>
2011-02-15 17:57:16 -0500

    Updated information.

--- setup.py
+++ setup.py
@@ -1,30 +1,35 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = 'django-metaimage',
-    version = '0.1',
-    url = '',
-    license = 'GPLv3',
-    description = 'Provides a useful subclass of photologue.ImageModel.',
-    long_description = read('README'),
-    author = 'Kai Wu',
-    author_email = 'k@limist.com',
-
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
-
-    classifiers = [
-	'Development Status :: 3 - Alpha',
-	'Framework :: Django',
-	'Intended Audience :: Developers',
-	'License :: OSI Approved :: GNU General Public License (GPL)',
-	'Operating System :: OS Independent',
+    name='django-metaimage',
+    version='0.2',
+    url='',
+    license='GPLv3',
+    description='Provides a useful subclass of photologue.ImageModel.',
+    long_description=read('README'),
+    author='Kai',
+    author_email='k@limist.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'setuptools',
+        'Django',
+        'django-photologue',
+        'django-tagging',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP'
-	]
+        ]
     )
