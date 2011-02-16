3c689bcd0cbda89d4988f9cc28e1bf24e1398e53
Kai <k@limist.com>
2011-02-15 19:27:37 -0500

    Minor PEP8 formatting changes.

--- src/metaimage/templatetags/metaimage_tags.py
+++ src/metaimage/templatetags/metaimage_tags.py
@@ -2,8 +2,9 @@ import re
 
 from django import template
 #from photologue.utils import EXIF
-register = template.Library()
 
+
+register = template.Library()
 
 
 @register.tag(name="print_exif")
@@ -17,7 +18,6 @@ def do_print_exif(parser, token):
     return PrintExifNode(exif)
 
 
-
 class PrintExifNode(template.Node):
 
     def __init__(self, exif):
@@ -28,13 +28,10 @@ class PrintExifNode(template.Node):
             exif = unicode(self.exif.resolve(context, True))
         except template.VariableDoesNotExist:
             exif = u''
-        EXPR =  "'(?P<key>[^:]*)'\:(?P<value>[^,]*),"
+        EXPR = "'(?P<key>[^:]*)'\:(?P<value>[^,]*),"
         expr = re.compile(EXPR)
-        msg  = "<table>"
+        msg = "<table>"
         for i in expr.findall(exif):
-            msg += "<tr><td>%s</td><td>%s</td></tr>" % (i[0],i[1])
-
+            msg += "<tr><td>%s</td><td>%s</td></tr>" % (i[0], i[1])
         msg += "</table>"
-
         return u'<div id="exif">%s</div>' % (msg)
-
