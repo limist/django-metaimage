a3517cb1720932b4b42d6071f774bbac63c6d39d
Kai <k@limist.com>
2011-02-23 14:29:35 -0500

    Minor changes.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version='0.3',
     url='',
     license='GPLv3',
-    description='Useful image-functionality on top of django-photologue.',
+    description='Useful image functionality wrapper around django-photologue.',
     long_description=read('README'),
     author='Kai',
     author_email='k@limist.com',
@@ -22,7 +22,7 @@ setup(
         'Django',
         'django-photologue',
         'django-tagging',
-        ],
+        'PIL'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
@@ -30,6 +30,5 @@ setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP'
-        ]
+        'Topic :: Internet :: WWW/HTTP']
     )
