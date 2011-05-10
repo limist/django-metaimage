8991c8ddb2a945c052d5422805467eba1f8d13e4
Kai <k@limist.com>
2011-05-10 12:00:33 -0400

    Fixed a known (but new to me) bug/limitation of m2m tag field in admin display.

--- src/metaimage/admin.py
+++ src/metaimage/admin.py
@@ -25,7 +25,15 @@ class MetaImageAdmin(BaseModelAdmin):
             'fields': ('image', 'crop_from', 'effect', 'title', 'slug', 'caption', 'source_url', 'source_note', 'safetylevel', 'privacy', 'tags', 'imageset', 'admin_notes', 'is_active')
             }),
         )
-    list_display = ('title', 'slug', 'caption', 'creator', 'created','is_public','safetylevel','tags',)
+    list_display = ('title', 'slug', 'caption', 'creator', 'created', 'is_public', 'safetylevel', 'the_tags')
     prepopulated_fields = {"slug": ("title",)}
+
+    def the_tags(self, obj):
+        """
+        This workaround of m2m limitation of django-taggit is from
+        https://github.com/alex/django-taggit/issues/46
+        """
+        return "%s" % (obj.tags.all(), )
+    the_tags.short_description = 'Tags'
 
 admin.site.register(MetaImage, MetaImageAdmin)
